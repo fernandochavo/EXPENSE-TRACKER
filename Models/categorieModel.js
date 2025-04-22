3498480dd1e744c97545a3401ee924d01754aa23
fernandochavo <106016753+fernandochavo@users.noreply.github.com>
2025-04-22 15:53:53 +0200

    create categories Model

---
+++ Models/categorieModel.js
@@ -0,0 +1,41 @@
+import db from '../db.js';
+
+async function createCategory(name,type,userId) {
+    const query = 'INSERT INTO categories (name, type, user_id) VALUES ($1, $2, $3) RETURNING *';
+    const values = [name, type, userId];
+    // Ensure that the userId is a valid integer
+    if (isNaN(userId)) {
+        throw new Error('Invalid user ID');
+    }
+    try {
+        const result = await db.query(query, values);
+        return result.rows[0]; // Return the newly created category
+    } catch (error) {
+        console.error('Error creating category:', error);
+        throw error; // Rethrow the error for handling in the calling function
+    }
+} 
+
+async function getAllCategories(userId) {
+    const query = 'SELECT * FROM categories WHERE user_id = $1';
+    const values = [userId];
+    try {
+        const result = await db.query(query, values);
+        return result.rows; // Return all categories for the user   
+    } catch (error) {
+        console.error('Error fetching categories:', error);
+        throw error; // Rethrow the error for handling in the calling function
+    }
+}
+
+async function getCategoryById(categoryId) {
+    const query = 'SELECT * FROM categories WHERE id = $1';
+    const values = [categoryId];
+    try {
+        const result = await db.query(query, values);
+        return result.rows[0]; // Return the category with the specified ID
+    } catch (error) {
+        console.error('Error fetching category:', error);
+        throw error; // Rethrow the error for handling in the calling function
+    }
+}
--- Models/userModel.js
+++ Models/userModel.js
@@ -4,7 +4,6 @@ import bcrypt from 'bcrypt';
 
 const saltRounds = 10;
 
-
 // User registration function
 async function registerUser(username, password) {
     const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -33,6 +32,29 @@ async function loginUser(username, password) {
         }
     }   catch (error) { 
         console.error('Error logging in user:', error);
+        throw error; // Rethrow the error for handling in the calling function
+    }
+}
+
+async function deleteUser(userId) {
+    const query = 'DELETE FROM users WHERE id = $1 RETURNING *';
+    const values = [userId];
+    try {
+        const result = await db.query(query, values);
+        return result.rows[0]; // Return the deleted user
+    }   catch (error) {         
+        console.error('Error deleting user:', error);
+        throw error; // Rethrow the error for handling in the calling function
+    }
+} 
+
+async function getAllUsers() {
+    const query = 'SELECT * FROM users';
+    try {
+        const result = await db.query(query);
+        return result.rows; // Return all users
+    } catch (error) {
+        console.error('Error fetching users:', error);
         throw error; // Rethrow the error for handling in the calling function
     }
 }
